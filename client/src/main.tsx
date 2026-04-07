4c6580ec27cc931e7d317ce8e2318653757b7e15
matt <mattschwartz@users.noreply.github.com>
2026-04-07 12:57:44 -0500

    spammy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { install as installErrorOverlay } from './console-error-overlay.ts'
 import App from './App.tsx'
+
+installErrorOverlay();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
